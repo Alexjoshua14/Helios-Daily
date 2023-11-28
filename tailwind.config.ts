96d0d24e6d982ff3e61a49087920eb0da143624b
Alex <ajoshua14@icloud.com>
2023-11-28 14:46:58 -0800

    Add shadcn-ui and add design system to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,65 @@
 import type { Config } from 'tailwindcss'
 
+const greyBlue = {
+  light: "hsl(233, 8%, 79%)",
+  dark: "hsl(240, 100%, 5%)"
+}
+
+const offWhite = "hsl(36, 100%, 99%)"
+
+/** @type {import('tailwindcss').Config} */
 const config: Config = {
+  darkMode: ["class"],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "white": offWhite,
+        "dark-blue": greyBlue["dark"],
+        "light-grey-blue": greyBlue["light"],
+        "soft-red": "hsl(5, 85%, 63%)",
+        "soft-orange": "hsl(35, 77%, 62%)",
+      },
+      textColor: {
+        "primary": greyBlue["dark"],
+        "secondary": greyBlue["dark"],
+        "tertiary": greyBlue["light"],
+        "light-primary": offWhite,
+        "light-secondary": greyBlue["light"],
+      },
+      fontSize: {
+        base: "15px"
+      },
+      backgroundColor: {
+        "primary": offWhite
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
 export default config
