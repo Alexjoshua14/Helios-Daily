f478c18057191a91dc24f2ebb76c2aa78ed1abb5
Alex <ajoshua14@icloud.com>
2023-11-28 18:38:22 -0800

    Add data

---
+++ src/lib/data.js
@@ -0,0 +1,48 @@
+
+export const headline = {
+  title: 'The Bright Future of Web 3.0?',
+  description: 'We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?',
+  link: '/',
+  imageMobile: '/images/image-web-3-mobile.jpg',
+  imageDesktop: '/images/image-web-3-desktop.jpg',
+  imageAlt: 'Web 3.0',
+}
+
+export const newArticles = [
+  {
+    title: 'Hydrogen VS Electric Cars',
+    description: 'Will hydrogen-fueled cars ever catch up to EVs?',
+    link: '/',
+  },
+  {
+    title: 'The Downsides of Al Artistry',
+    description: 'What are the possible adverse effects of on-demand Al image generation?',
+    link: '/',
+  },
+  {
+    title: 'Is VC Funding Drying Up?',
+    description: 'Private funding by VC firms is down 50% YOY. We take a look at what that means.',
+    link: '/',
+  },
+]
+
+export const trendingArticles = [
+  {
+    title: 'Reviving Retro PCs',
+    description: 'What happens when old PCs are given modern upgrades?',
+    link: '/',
+    image: '/images/image-retro-pcs.jpg',
+  },
+  {
+    title: 'Top 10 Laptops of 2022',
+    description: 'Our best picks for various needs and budgets.',
+    link: '/',
+    image: '/images/image-top-laptops.jpg',
+  },
+  {
+    title: 'The Growth of Gaming',
+    description: 'How the pandemic has sparked fresh opportunities.',
+    link: '/',
+    image: '/images/image-gaming-growth.jpg',
+  }
+]
